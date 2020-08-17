7eb96f5c7ddaf3982912f842eb107574b40d03fc
yekeren <yekeren.cn@gmail.com>
2020-08-17 15:32:14 -0400

    format scene graph json output

--- tools/SceneGraphDemo.java
+++ tools/SceneGraphDemo.java
@@ -5,20 +5,31 @@ import edu.stanford.nlp.scenegraph.RuleBasedParser;
 import edu.stanford.nlp.scenegraph.SceneGraph;
 
 public class SceneGraphDemo {
+
   public static void main(String[] args) throws IOException {
+
+    String line;
     Scanner scanner = new Scanner(System.in);
     RuleBasedParser parser = new RuleBasedParser();
 
-    String line;
-    while ((line = scanner.nextLine()) != null) {
+    System.err.println("Processing from stdin. Enter one sentence per line.");
+    System.err.print("> ");
+
+    Integer count = 0;
+    while (scanner.hasNextLine()) {
+      line = scanner.nextLine();
       SceneGraph scene = parser.parse(line);
 
-      //printing the scene graph in a readable format
-      System.out.println(scene.toReadableString()); 
+      // Printing the scene graph in a readable format.
+      System.err.println(scene.toReadableString()); 
       
-      //printing the scene graph in JSON form
-      System.out.println(scene.toJSON(0, "", "")); 
+      // Printing the scene graph in JSON form.
+      // Parameteres are id, url, and phrase.
+      System.out.println(scene.toJSON(count, "", line)); 
+
+      System.err.print("> ");
+      count += 1;
     }
-    System.out.println("Done");
   }
+
 }
--- tools/download_scene_graph_parser.sh
+++ tools/download_scene_graph_parser.sh
@@ -2,7 +2,6 @@
 
 set -o errexit
 set -o nounset
-set -x
 
 download() {
   local -r dir=$1
@@ -22,6 +21,7 @@ fi
 # Create the directory if it does not exist.
 data_dir=$1
 mkdir -p "${data_dir}"
+data_dir_full="`readlink -f ${data_dir}`"
 
 # Download Stanford corenlp 3.6.0.
 download "https://nlp.stanford.edu/software" "stanford-corenlp-full-2015-12-09.zip" "${data_dir}"
@@ -32,4 +32,15 @@ cd "${data_dir}/stanford-corenlp-full-2015-12-09"
 download "https://nlp.stanford.edu/projects/scenegraph" "scenegraph-1.0.jar" "."
 
 # Download json-simple jar file.
-download "http://json-simple.googlecode.com/files" "json-simple-1.1.1.jar" "."
+download "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/json-simple" "json-simple-1.1.1.jar" "."
+
+
+# Show information to setup java CLASSPATH.
+cd ..
+echo -e "\n\n\n"
+echo "Commands to test the Stanford Scene Graph Parser:"
+echo "export CLASSPATH=\"$CLASSPATH:${data_dir_full}:${data_dir_full}/stanford-corenlp-full-2015-12-09/*\""
+
+echo "cd \"${data_dir}\""
+echo "javac \"SceneGraphDemo.java\""
+echo "java -mx2g \"SceneGraphDemo\""
