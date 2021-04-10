6beebbfc1de6e5191bdd2facb69bffb2f3bb55f9
yekeren <yekeren.cn@gmail.com>
2021-04-10 11:12:56 -0400

    rm two files

--- prepare.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -o errexit
-set -o nounset
-set -x
-
-git clone "https://github.com/tensorflow/models.git" "tensorflow_models"
-ln -s "tensorflow_models/research/object_detection" .
